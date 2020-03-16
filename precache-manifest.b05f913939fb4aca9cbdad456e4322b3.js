75b2cdf4cbfdcb10eb9db1fe90fdaf7146993340
Arun Kumar <arun.kumar@freolabs.com>
2020-03-16 18:59:15 +0530

    Updates

---
+++ precache-manifest.b05f913939fb4aca9cbdad456e4322b3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e17ffabd0b5f3806dfc053b6afad78",
+    "url": "/corona-informatica/index.html"
+  },
+  {
+    "revision": "11938cb5852241b825ae",
+    "url": "/corona-informatica/static/css/main.ff3960f8.chunk.css"
+  },
+  {
+    "revision": "187f5aebc05514acad6b",
+    "url": "/corona-informatica/static/js/2.3b38eefb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/corona-informatica/static/js/2.3b38eefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11938cb5852241b825ae",
+    "url": "/corona-informatica/static/js/main.3ecb41be.chunk.js"
+  },
+  {
+    "revision": "b13a58ea933dc9b52b3a",
+    "url": "/corona-informatica/static/js/runtime-main.ec802709.js"
+  },
+  {
+    "revision": "18a547a649d3cd03e7b4f8e1e433c349",
+    "url": "/corona-informatica/static/media/1.18a547a6.jpg"
+  },
+  {
+    "revision": "8f95c3462abc698639bbb3fa453ed460",
+    "url": "/corona-informatica/static/media/1.8f95c346.png"
+  },
+  {
+    "revision": "c2a48804864578f2fd714f14866adc30",
+    "url": "/corona-informatica/static/media/1.c2a48804.png"
+  },
+  {
+    "revision": "c860bbf54f41b49c4dd8f0e8673dbea5",
+    "url": "/corona-informatica/static/media/10.c860bbf5.png"
+  },
+  {
+    "revision": "c20b91b2725be99adc44ef0ce6f52c0a",
+    "url": "/corona-informatica/static/media/11.c20b91b2.png"
+  },
+  {
+    "revision": "e30dfa7f1e61579668fd035cc1af1cd4",
+    "url": "/corona-informatica/static/media/12.e30dfa7f.png"
+  },
+  {
+    "revision": "90e9db9fad3d1c9eda5f3a2e37f66c29",
+    "url": "/corona-informatica/static/media/13.90e9db9f.png"
+  },
+  {
+    "revision": "47a59d0bb9f71dad2144506afaa1fbba",
+    "url": "/corona-informatica/static/media/2.47a59d0b.jpg"
+  },
+  {
+    "revision": "7bca717fe28ec4e73a2ec84448f6f994",
+    "url": "/corona-informatica/static/media/2.7bca717f.png"
+  },
+  {
+    "revision": "b5cd1c6553b67850c6a3275cc1b80c2e",
+    "url": "/corona-informatica/static/media/2.b5cd1c65.png"
+  },
+  {
+    "revision": "3fd044dc540c9eb96c997efa266137c7",
+    "url": "/corona-informatica/static/media/3.3fd044dc.png"
+  },
+  {
+    "revision": "b26a75098eb71e040b1b49e02f58dc3c",
+    "url": "/corona-informatica/static/media/3.b26a7509.png"
+  },
+  {
+    "revision": "00f14d54f46526af68d30a849b21d27a",
+    "url": "/corona-informatica/static/media/4.00f14d54.png"
+  },
+  {
+    "revision": "a333b1efed67f9ec585d7dc1879cf688",
+    "url": "/corona-informatica/static/media/4.a333b1ef.png"
+  },
+  {
+    "revision": "42d5ca3688d644eda4d07d49670bec2b",
+    "url": "/corona-informatica/static/media/5.42d5ca36.png"
+  },
+  {
+    "revision": "b988c7faf53fb689ba81713ed22f2bbe",
+    "url": "/corona-informatica/static/media/6.b988c7fa.png"
+  },
+  {
+    "revision": "1320c3abdbd83a19b3c42dedd80097d3",
+    "url": "/corona-informatica/static/media/7.1320c3ab.png"
+  },
+  {
+    "revision": "25fffabceb570b1cca67ef543d3f7472",
+    "url": "/corona-informatica/static/media/8.25fffabc.png"
+  },
+  {
+    "revision": "7a55c8b49509a8b570a2f8ab353cc78c",
+    "url": "/corona-informatica/static/media/9.7a55c8b4.png"
+  },
+  {
+    "revision": "d72e7e8ea5ca6ce1406cda9f3058edaf",
+    "url": "/corona-informatica/static/media/corona-image.d72e7e8e.png"
+  },
+  {
+    "revision": "1090bee7e7928cfc153b872bf71a578f",
+    "url": "/corona-informatica/static/media/corona-spread.1090bee7.jpg"
+  },
+  {
+    "revision": "f384e2356aca6fe483f5fd5d0d5fad35",
+    "url": "/corona-informatica/static/media/covid-19-banner.f384e235.png"
+  }
+]);
